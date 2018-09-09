a52e7cdfd318b105c65f5cc25d6d871d24d33027
Daniels Kursits <daniels.kursits@gmail.com>
2018-09-10 00:36:10 +0300

    Initial commit

---
+++ wiktor/js/syntax.js
@@ -0,0 +1,71 @@
+function findall(regex_pattern, string_, typename) {
+   var output_list = [];
+   while (true) {
+      var a_match = regex_pattern.exec(string_);
+      if (a_match) {
+         output_list.push([typename, a_match[0], a_match.index]);
+      } else {
+         break;
+      }
+   }
+   return output_list;
+}
+
+function highlight(source, tokens, i = 0, last = 0) {
+   return tokens[i]
+      ? source.slice(last, tokens[i][2])
+      + '<span class=' + tokens[i][0] + '>' + tokens[i][1] + '</span>'
+      + highlight(source, tokens, i + 1, tokens[i][2] + tokens[i][1].length)
+      : '';
+}
+
+function compose(source, tokens) {
+   var tokens = [].concat(...tokens).sort((a, b) => a[2] >= b[2]);
+   var final = [];
+   for (var i = 0; tokens[i]; i++) {
+      final.push(tokens[i])
+      if (tokens[i + 1] && tokens[i][2] == tokens[i + 1][2]) {
+         var j = i + 1;
+         while (tokens[i][2] + tokens[i][1].length > tokens[j][2] + tokens[j][1].length) j++;
+         i = j;
+      }
+   }
+   return highlight(source.innerText, final)
+}
+
+function regEscape(string) {
+   return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); 
+}
+
+$.loadLanguage = function (language, success) {
+   language = 'wiktor/js/' + language.replace('-', '/') + '.json';
+   return $.getJSON(language, null, function (data) {
+
+      data.keywords = new RegExp('\\b(' + data.keywords.join('|') + ')\\b', 'gm')
+      data.punctuation = new RegExp('(' + data.punctuation.split('').map(regEscape).join('|') + ')', 'gm')
+      data.comment = new RegExp('(' +
+         data.comment.map(
+            (pair) => '(' + regEscape(pair[0]) + '(.|\\s)*?' + regEscape(pair[1]) + ')'
+         ).join('|') + ')', 'gm')
+      data.string = new RegExp('(' +
+         data.string.map(
+            (pair) => '(' + regEscape(pair[0]) + '(\\\\\\\\|\\\\' + regEscape(pair[1]) + '|.|\\s)*?' + regEscape(pair[1]) + ')'
+         ).join('|') + ')', 'gm')
+
+      success(data)
+   });
+};
+
+$(function () {
+   $('code').each(function (_, code) {
+      if (code.className) {
+         $.loadLanguage(code.className, function (lang) {
+            var keywords = findall(lang.keywords, code.innerText, 'keyword')
+            var punctuation = findall(lang.punctuation, code.innerText, 'punctuation')
+            var comments = findall(lang.comment, code.innerText, 'comment')
+            var strings = findall(lang.string, code.innerText, 'string')
+            code.innerHTML = compose(code, [punctuation, keywords, strings, comments]);
+         })
+      }
+   })
+})
---
+++ wiktor/wiktor.js
@@ -0,0 +1,105 @@
+var fadetime = 100;
+
+function mktree(json, path = []) {
+   var chunk = '';
+
+   if (Array.isArray(json)) {
+      chunk = $(document.createDocumentFragment());
+      json.forEach(function (e) { mktree(e, path).appendTo(chunk) });
+
+   } else if (typeof (json) === typeof ({})) {
+      chunk = $('<ul></ul>');
+      for (var key in json) {
+         $("<a id='" + key + "'><li class='sep'>" + key + "</li></a>").appendTo(chunk);
+         var inner = $('<ul></ul>');
+         path.push(key);
+         mktree(json[key], path).appendTo(inner);
+         path.pop();
+         inner.appendTo(chunk);
+      }
+
+   } else {
+      path = path.join("-");
+      path = path.length > 0 && path + "-" || '';
+
+      chunk = $("<a href='#" + path + json + "'><li>" + json + "</li></a>")
+         .on('click', function () {
+            if (!$('#' + path + json).is(':visible')) {
+               $('#' + path + json).appendTo('content').fadeIn(fadetime);
+            }
+         });
+      ;
+   }
+
+   return chunk;
+}
+
+function mkentry(json, path = [], defer = []) {
+   if (Array.isArray(json)) {
+      json.forEach(function (e) { mkentry(e, path, defer) });
+
+   } else if (typeof (json) === typeof ({})) {
+      for (var key in json) {
+         path.push(key);
+         mkentry(json[key], path.slice(), defer);
+         path.pop();
+      }
+
+   } else {
+      defer.push($.get(
+         'entries/' + path.join('/') + '/' + json + '.md',
+         function (data) {
+            title = path.join(" · ");
+            title = title.length > 0 && title + " · " || '';
+            path = path.join("-");
+            path = path.length > 0 && path + "-" || '';
+
+            var entryHtml = $(
+               '<entry id=' + path + json + ' style="display:none">'
+               + '<h1>' + title + json + '<close></close></h1>'
+               + marked(data)
+               + '</entry>'
+            ).appendTo('content');
+
+            $('close', entryHtml).on('click', function () {
+               entryHtml.fadeOut(100);
+            });
+
+            if (window.location.hash.substr(1) == entryHtml.prop('id')) {
+               if (!entryHtml.is(':visible')) {
+                  entryHtml.appendTo('content').fadeIn(fadetime);
+               }
+            }
+         })
+      )
+   }
+
+   return defer;
+}
+
+$(function () {
+   $.getScript("entries.js", function () {
+      mktree(entries).appendTo('links');
+      var dentries = mkentry(entries);
+      dentries.forEach(function (entry) {
+         entry.done(function () {
+            $.getScript("wiktor/js/syntax.js");
+
+         })
+      })
+
+      $.when(...dentries)
+         .done(function () {
+            if ($('content')[0].childElementCount == 0) {
+               $('empty').fadeIn(fadetime);
+            }
+            $('nav').fadeIn(fadetime);
+         })
+         .fail(function () {
+            if ($('content')[0].childElementCount == 0) {
+               $('empty').fadeIn(fadetime);
+            }
+            $('nav').fadeIn(fadetime);
+         })
+   })
+})
