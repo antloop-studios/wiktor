cffa7c8760136da122a058799256ee41a474d2a4
Daniels Kursits <daniels.kursits@gmail.com>
2018-09-15 01:56:18 +0300

    Clean up formatting, optimize highlight code

--- wiktor/wiktor.js
+++ wiktor/wiktor.js
@@ -3,38 +3,42 @@
    MIT license
 */
 
-
 var fadetime = 100;
 
 function mktree(json, path = []) {
-   var chunk = '';
+   var chunk = "";
 
    if (Array.isArray(json)) {
       chunk = $(document.createDocumentFragment());
-      json.forEach(function (e) { mktree(e, path).appendTo(chunk) });
-
-   } else if (typeof (json) === typeof ({})) {
-      chunk = $('<ul></ul>');
+      json.forEach(function(e) {
+         mktree(e, path).appendTo(chunk);
+      });
+   } else if (typeof json === typeof {}) {
+      chunk = $("<ul></ul>");
       for (var key in json) {
-         $("<a id='" + key + "'><li class='sep'>" + key + "</li></a>").appendTo(chunk);
-         var inner = $('<ul></ul>');
+         $("<a id='" + key + "'><li class='sep'>" + key + "</li></a>").appendTo(
+            chunk
+         );
+         var inner = $("<ul></ul>");
          path.push(key);
          mktree(json[key], path).appendTo(inner);
          path.pop();
          inner.appendTo(chunk);
       }
-
    } else {
       path = path.join("-");
-      path = path.length > 0 && path + "-" || '';
+      path = (path.length > 0 && path + "-") || "";
 
-      chunk = $("<a href='#" + path + json + "'><li>" + json + "</li></a>")
-         .on('click', function () {
-            if (!$('#' + path + json).is(':visible')) {
-               $('#' + path + json).appendTo('content').fadeIn(fadetime);
+      chunk = $("<a href='#" + path + json + "'><li>" + json + "</li></a>").on(
+         "click",
+         function() {
+            if (!$("#" + path + json).is(":visible")) {
+               $("#" + path + json)
+                  .appendTo("content")
+                  .fadeIn(fadetime);
             }
-         });
-      ;
+         }
+      );
    }
 
    return chunk;
@@ -42,70 +46,83 @@ function mktree(json, path = []) {
 
 function mkentry(json, path = [], defer = []) {
    if (Array.isArray(json)) {
-      json.forEach(function (e) { mkentry(e, path, defer) });
-
-   } else if (typeof (json) === typeof ({})) {
+      json.forEach(function(e) {
+         mkentry(e, path, defer);
+      });
+   } else if (typeof json === typeof {}) {
       for (var key in json) {
          path.push(key);
          mkentry(json[key], path.slice(), defer);
          path.pop();
       }
-
    } else {
-      defer.push($.get(
-         'entries/' + path.join('/') + '/' + json + '.md',
-         function (data) {
-            title = path.join(" · ");
-            title = title.length > 0 && title + " · " || '';
-            path = path.join("-");
-            path = path.length > 0 && path + "-" || '';
+      defer.push(
+         $.get(
+            "entries/" + path.join("/") + "/" + json + ".md",
+            { _: $.now() },
+            function(data) {
+               title = path.join(" · ");
+               title = (title.length > 0 && title + " · ") || "";
+               path = path.join("-");
+               path = (path.length > 0 && path + "-") || "";
 
-            var entryHtml = $(
-               '<entry id=' + path + json + ' style="display:none">'
-               + '<h1>' + title + json + '<close></close></h1>'
-               + marked(data)
-               + '</entry>'
-            ).appendTo('content');
+               var entryHtml = $(
+                  "<entry id=" +
+                     path +
+                     json +
+                     ' style="display:none">' +
+                     "<h1>" +
+                     title +
+                     json +
+                     "<close></close></h1>" +
+                     marked(data) +
+                     "</entry>"
+               ).appendTo("content");
 
-            $('close', entryHtml).on('click', function () {
-               entryHtml.fadeOut(100);
-            });
+               $("close", entryHtml).on("click", function() {
+                  entryHtml.fadeOut(100);
+               });
 
-            if (window.location.hash.substr(1) == entryHtml.prop('id')) {
-               if (!entryHtml.is(':visible')) {
-                  entryHtml.appendTo('content').fadeIn(fadetime);
+               if (
+                  entryHtml
+                     .prop("id")
+                     .startsWith(window.location.hash.substr(1))
+               ) {
+                  if (!entryHtml.is(":visible")) {
+                     entryHtml.appendTo("content").fadeIn(fadetime);
+                  }
                }
             }
-         })
-      )
+         )
+      );
    }
 
    return defer;
 }
 
-$(function () {
-   $.getScript("entries.js", function () {
-      mktree(entries).appendTo('links');
+$(function() {
+   $.getScript("entries.js", function() {
+      mktree(entries).appendTo("links");
       var dentries = mkentry(entries);
-      dentries.forEach(function (entry) {
-         entry.done(function () {
-            $.getScript("wiktor/js/syntax.js");
 
-         })
-      })
+      dentries.forEach(function(entry) {
+         entry.done(function() {
+            $("code").each((_, code) => highlight(code));
+         });
+      });
 
       $.when(...dentries)
-         .done(function () {
-            if ($('content')[0].childElementCount == 0) {
-               $('#empty').fadeIn(fadetime);
+         .done(function() {
+            if ($("content")[0].childElementCount == 0) {
+               $("#empty").fadeIn(fadetime);
             }
-            $('nav').fadeIn(fadetime);
+            $("nav").fadeIn(fadetime);
          })
-         .fail(function () {
-            if ($('content')[0].childElementCount == 0) {
-               $('#empty').fadeIn(fadetime);
+         .fail(function() {
+            if ($("content")[0].childElementCount == 0) {
+               $("#empty").fadeIn(fadetime);
             }
-            $('nav').fadeIn(fadetime);
-         })
-   })
-})
+            $("nav").fadeIn(fadetime);
+         });
+   });
+});
